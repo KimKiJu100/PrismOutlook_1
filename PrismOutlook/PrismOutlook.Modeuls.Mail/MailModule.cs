69d2324637e98e9659013d3bff55445f6eb643cc
kkj <goodkkj96@gmail.com>
2025-07-11 18:10:06 +0900

    Moudule 추가 중...

--- PrismOutlook/PrismOutlook.Modeuls.Mail/MailModule.cs
+++ PrismOutlook/PrismOutlook.Modeuls.Mail/MailModule.cs
@@ -1,15 +1,22 @@
 ﻿using Prism.Ioc;
 using Prism.Modularity;
 using Prism.Regions;
+using PrismOutlook.Core;
 using PrismOutlook.Modeuls.Mail.Views;
 
 namespace PrismOutlook.Modeuls.Mail
 {
     public class MailModule : IModule
     {
+        private readonly IRegionManager _regionManager;
+
+        public MailModule(IRegionManager regionManager)
+        {
+            this._regionManager = regionManager;
+        }
         public void OnInitialized(IContainerProvider containerProvider)
         {
-
+            this._regionManager.RegisterViewWithRegion(RegionNames.ContentRegion,typeof(ViewA));
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
